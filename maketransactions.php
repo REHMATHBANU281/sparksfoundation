0a9f61262908676307c271460a86c54b8d8a8909
REHMATHBANU281 <86918582+REHMATHBANU281@users.noreply.github.com>
2021-07-13 17:46:48 +0530

    Update maketransactions.php

--- maketransactions.php
+++ maketransactions.php
@@ -14,8 +14,8 @@ include "navbar2.php";
 	<center>
 	<h1 style="color:#cc99ff ;">Make Transactions</h1>
 	<form name="form" method="post" onsubmit="return validate();" action="maketransback.php" autocomplete="off">
-		<input type="text" name="a" placeholder="Enter Sender account number">
-		<input type="text" name="b" placeholder="Enter Receiver account number">
+		<input type="text" name="a" placeholder="Enter Name of sender">
+		<input type="text" name="b" placeholder="Enter name of receiver">
 		<input type="text" name="c" placeholder="Enter amount (in rupees)"><br>
 		<input type="submit" value ="Send">
 		</form>
