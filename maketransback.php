5f7417875e3e658b3459d6fbe3cc5ea6ccf36dd0
REHMATHBANU281 <86918582+REHMATHBANU281@users.noreply.github.com>
2021-07-13 17:50:26 +0530

    Update maketransback.php

--- maketransback.php
+++ maketransback.php
@@ -5,11 +5,37 @@ $receiver = $_POST['b'];
 $money = $_POST['c'];
 $fetch1=mysqli_query($database,"select *from customers where customername='$sender' ");
 $sent = mysqli_fetch_assoc($fetch1);
+if(mysqli_num_rows($fetch1)==0){
+	echo "Sender not found";
+	echo '<a href="maketransactions.php">Go back</a>';
+}
+
 $fetch2 = mysqli_query($database,"select *from customers where customername='$receiver' ");
-$received = mysqli_fetch_assoc($fetch2);
+//$received = mysqli_fetch_assoc($fetch2);
+if(mysqli_num_rows($fetch2)==0){
+	echo "Receiver not found";
+	echo '<a href="maketransactions.php">Go back</a>';
+}
 $balance = $sent['bankbalance'];
 if($balance<$money){
 	echo "insufficient balance";
 }
 
+else
+{
+	
+	$id =uniqid();
+	$date = date('Y-m-d H:i:s'); 
+	$insert = mysqli_query($database,"insert into transfers values('$sender','$receiver','$date','$money','$id')");
+	if($insert)
+	{
+		$deduct = mysqli_query($database,"update customers set bankbalance=bankbalance-$money where customername='$sender' ");
+		$credit = mysqli_query($database,"update customers set bankbalance=bankbalance+$money where customername='$receiver'");
+
+		if($deduct && $credit)
+		{
+			header('location:transactions.php');
+		}
+	}
+}
 ?>
